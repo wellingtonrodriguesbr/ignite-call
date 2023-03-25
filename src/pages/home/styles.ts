97ed149ca1365d626debd831ac0080cbdfec6f1f
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-03-25 11:09:01 -0300

    feat: WIP home page

---
+++ src/pages/home/styles.ts
@@ -0,0 +1,38 @@
+import { Heading, styled, Text } from '@ui-ignite/react'
+
+export const Container = styled('div', {
+  maxWidth: 'calc(100vw - (100vw - 1160px) / 2)',
+  height: '100vh',
+  marginLeft: 'auto',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '$20',
+  overflow: 'hidden',
+
+  img: {
+    zIndex: -1,
+  },
+})
+
+export const Hero = styled('div', {
+  maxWidth: 480,
+  padding: '0 $10',
+
+  [`${Heading}`]: {
+    '@media(max-width: 600px)': {
+      fontSize: '$6xl',
+    },
+  },
+  [`${Text}`]: {
+    color: '$gray200',
+    marginTop: '$2',
+  },
+})
+export const Preview = styled('div', {
+  paddingRight: '$8',
+
+  '@media(max-width: 600px)': {
+    display: 'none',
+  },
+})
