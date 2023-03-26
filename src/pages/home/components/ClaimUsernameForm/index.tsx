4c33c62a6591849120e3d37c83422ca1758c7719
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-03-26 10:00:15 -0300

    feat: WIP form to register username

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -2,11 +2,29 @@ import { Button, TextInput } from '@ui-ignite/react'
 import { Form } from './styles'
 
 import { ArrowRight } from '@phosphor-icons/react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const claimUsernameFormSchema = z.object({
+  username: z.string(),
+})
+
+type claimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<claimUsernameFormData>()
+
+  async function handleClaimUsername(data: claimUsernameFormData) {
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
-      <TextInput prefix="call.com/" placeholder="seu-usuario" />
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+      <TextInput
+        prefix="call.com/"
+        placeholder="seu-usuario"
+        {...register('username')}
+      />
       <Button type="submit">
         Reservar usúario
         <ArrowRight />
