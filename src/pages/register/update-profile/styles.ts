c54e07666b1569df8e134b971ce6d4612ede0d52
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-04-08 11:15:57 -0300

    feat: WIP update profile page

---
+++ src/pages/register/update-profile/styles.ts
@@ -0,0 +1,17 @@
+import { Box, Text, styled } from '@ui-ignite/react'
+
+export const ProfileBox = styled(Box, {
+  marginTop: '$6',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '$4',
+
+  label: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '$2',
+  },
+})
+export const FormAnnotation = styled(Text, {
+  color: '$gray200',
+})
